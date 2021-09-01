a8cf7d2f60658034f6558705a6bd3903bbd7afc6
eturakaki <kakifaure@gmail.com>
2021-09-01 20:27:37 -0300

    Error en forEach

--- Js/productosApp.js
+++ Js/productosApp.js
@@ -1,78 +1,125 @@
 
 
-const contenedor = document.getElementById("contenedor")
-const tableBody = document.getElementById("tableBody")
+const contenedor = document.getElementById("contenedor");
+const tableBody = document.getElementById("tableBody");
+let carrito = [];
+const botonBorrarCarrito = document.getElementById('btnBorrar');
+  const botonComprarCarrito = document.getElementById('btnComprar');
+const botonBorrar = document.getElementById('botonBorrar');
+const botonAniadir = document.getElementById('botonAniadir');
+const footerCarrito = document.getElementById('footerCarrito');
+const totalCarrito = document.getElementById('totalCarrito');
 
 
 
-stockProductos.forEach ((prod) => {
+
+stockProductos.forEach ((producto) => {
   let divProd = document.createElement('div')
   divProd.className = "card"
-  divProd.style = "width: 18rem;"
-  divProd.innerHTML = `
-      <img src=" ${prod.img}" class="card-img-top imagenProd">
+  divProd.style = "width: 18rem; margin: 30px;"
+  divProd.innerHTML += `
+      <img src=" ${producto.img}" class="card-img-top imagenProd">
       <div class="card-body">
-        <h5 class="card-title">${prod.nombre}</h5>
-        <h6 class="card-subtitle mb-2 text-muted">${"Autor: " + prod.autor}</h6>
-        <p class="card-text">${prod.desc}.</p>
-        <p><strong class="card-text">${ "Precio: $" + prod.precio}</strong></p>
-        
-        <button id="btn" onclick="agregarAlCarrito(${prod.id})" class="btn btn-primary">Agregar al carrito</button>
+        <h5 class="card-title">${producto.nombre}</h5>
+        <h6 class="card-subtitle mb-2 text-muted">${"Autor: " + producto.autor}</h6>
+        <p class="card-text">${producto.desc}.</p>
+        <p><strong class="card-text">${ "Precio: $" + producto.precio}</strong></p>
+        <p class="card-text" style="text-align: ;"> (${ producto.stock + " disponibles"})</p>
+        <button id="btn" onclick="agregarAlCarrito(${producto.id}) " class="btn btn-primary">Agregar al carrito</button>
       </div>
+      
     `
 
     contenedor.appendChild(divProd)
 })
 
 
-const carrito = []
 
 const mostrarCompra = () => {
 
   tableBody.innerHTML = ""
    
-  carrito.forEach((prod) =>{
+  carrito.forEach((producto) =>{
+    
     const tr = document.createElement('tr')
     tr.innerHTML = `
-             <th scope="row">${prod.id}</th>
-              <td>${prod.nombre}</td>
-              <td>${prod.autor}</td>
-              <td>${prod.precio}</td>
+             <th scope="row">${producto.id}</th>
+              <td>${producto.nombre}</td>
+              <td>${producto.autor}</td>
+              <td>${producto.cantidad}</td>
+              <td>
+              <button type="button" class="btn btn-success  " id="botonAniadir">+</button>
+              <button type="button" class="btn btn-danger" id="botonBorrar">-</button>
+              </td>
+              <td>${"$ " + producto.precio * producto.cantidad}</td>
 
 
              
     `
-    tableBody.appendChild(tr)          
+    
+    tableBody.appendChild(tr)
+    
   })
 
+  pintarFooter()
+  
 }
-
-
 
 
 function agregarAlCarrito (prodId) {
   let producto = stockProductos.find( (el) => el.id === prodId )
 
-  carrito.push(producto)
+  if (producto.stock > 0){
 
-  mostrarCompra()
+    if(carrito.hasOwnProperty(producto.id)){
+
+      producto.cantidad = carrito[producto.id].cantidad + 1
+    }
+    carrito[producto.id] = {...producto}
+    
+    mostrarCompra()
+
+  }else{
+    alert("No hay stock")
+  }
+  
+  
 }
 
-const botonBorrarCarrito = document.getElementById('btnBorrar')
-const botonComprarCarrito = document.getElementById('btnComprar')
+const pintarFooter = () => {
 
+  footerCarrito.innerHTML = ''
+
+  if(Object.keys(carrito).length === 0){
+
+    footerCarrito.innerHTML = `<th scope="row" colspan="6">Carrito vacío - comience a comprar!</th>`
+
+    return
+  }
+  const nPrecio = Object.values(carrito).reduce((acc, {cantidad, precio}) => acc + cantidad * precio, 0 )
+  const nCantidad = Object.values(carrito).reduce( (acc , {cantidad}) => acc + cantidad ,0)
+  
+  totalCarrito.querySelectorAll('th')[1].textContent = nCantidad
+  totalCarrito.querySelectorAll('td')[1].textContent = nPrecio
+  
+console.log(carrito)
+}
 
 
 botonBorrarCarrito.addEventListener('click', () => {
 
-  tableBody.innerHTML = ""
-  carrito = []
+  carrito = {}
+  tableBody.innerHTML = ''
+  agregarAlCarrito()
+  
   
 
 } )
-
 botonComprarCarrito.addEventListener('click', () => {
-  
-    alert("Compraste el Carrito")
+
+  alert("Compraste el Carrito")
+  tableBody.innerHTML = ''
+  agregarAlCarrito()
 } )
+
 
---
+++ apis/api.json
@@ -0,0 +1,12 @@
+let stockProductos = [
+    {id: 1, nombre: "Contabilidad Superior", tipo: "Libro", desc: "Ingresos en general", precio:  4300,autor: "Enrique Fowler Newton" ,img: "../img_stock/1.png" },
+    {id: 2, nombre: "Contabilidad básica", tipo: "Libro", desc: "Contabilidad basica", precio: 3000, autor: "Enrique Fowler Newton", img: "../img_stock/2.jpg" },
+    {id: 3, nombre: "Contabilidad y finanzas para Dummies", tipo: "Libro", desc: "Introducción a esta disciplina", precio: 4000, autor: "Oriol Amat", img: "../img_stock/3.jpg" },
+    {id: 4, nombre: "Excel contable", tipo: "Libro", desc: "Excel", precio: 2500, autor: "César Oceda Samaniego", img: "../img_stock/4.jpg" },
+    {id: 5, nombre: "Introducción a la contabilidad y las finanzas NE", tipo: "Libro", desc: "Introducción a contabilidad", precio: 4000, autor: "Oriol Amat", img: "../img_stock/5.jpg" },
+    {id: 6, nombre: "Contabilidad y gestión de costes", tipo: "Libro", desc: "La contabilidad de costes", precio: 5000, autor: "Oriol Amat y Pilar Soldevila García", img: "../img_stock/6.jpg" },
+    {id: 7, nombre: "Finanzas para emprendedores", tipo: "Libro", desc: "Finanzas", precio: 3500, autor: "Antonio Manzanera", img: "../img_stock/7.jpg" },
+    {id: 8, nombre: "Manual Básico de Contabilidad: El Arte de la lógica Contable", tipo: "Libro", desc: "Introducción a contabilidad", precio: 4000, autor: "VEGA", img: "../img_stock/8.jpg" },
+    {id: 9, nombre: "Contabilidad para numerofóbicos", tipo: "Libro", desc: " guía de supervivencia para propietarios", precio: 3780, autor: "Dawn Fotopulos", img: "../img_stock/9.jpg" },
+    {id: 10, nombre: "La ciencia del bienestar", tipo: "Libro", desc: "Los 5 elementos esenciales", precio: 7000, autor: "Tom Rath", img: "../img_stock/10.jpg" },
+]
